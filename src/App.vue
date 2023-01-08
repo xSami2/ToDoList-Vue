90a4b2b91735ea104d07141306dce64a29ce0651
xSami2 <109634088+xSami2@users.noreply.github.com>
2023-01-08 06:31:48 +0300

    Category Done!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
 import  {computed, reactive, ref } from "vue";
-import {ChevronDownIcon} from '@heroicons/vue/24/solid'
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 
 import Button from "@/components/Button.vue";
 
@@ -10,16 +8,19 @@ let HomeWorks = reactive([
   {
     id : 1 ,
     name : "Math HomeWork",
+    category :'School',
     status : false
   },
   {
     id : 2 ,
     name : "Phys HomeWork",
+    category :'School',
     status : false
   },
   {
     id : 3 ,
     name : "History HomeWork",
+    category :'School',
     status : false
   },
 
@@ -30,7 +31,10 @@ let CompletedHomeworks = computed( () => {
 let unCompletedHomeworks = computed( () => {
   return HomeWorks.filter(a => !a.status).length
 }); // Show unCompleted Homework
+let Category = ['University','Work','Personal']
+
 let HomeWorkAdd = ref('');
+let selected = ref('');
 
 function ResetInput() {
   HomeWorkAdd.value = '';
@@ -41,15 +45,15 @@ function DeleteAllTask() {
     delete HomeWorks[i]
   }
 }
-function AddTask(HomeWorkAdd ){
-  if(HomeWorkAdd.length === 0 )
-  {
+function AddTask(HomeWorkAdd,selected ){
+  if(HomeWorkAdd.length === 0 ) {
     alert("Insert Something pls")
   }
   else {
     HomeWorks.push({
       id : HomeWorks.length+1 ,
       name : HomeWorkAdd ,
+      category :selected,
       status : false
     })
     ResetInput()
@@ -64,15 +68,23 @@ function AddTask(HomeWorkAdd ){
 
   <div>
     <h2 class="font-bold mb-2 text-left" > inProgress</h2>
-    <ul  v-show="unCompletedHomeworks" v-for="item in HomeWorks.filter(item => !item.status)" :key="item.id" class="grid grid-cols-2 ">
+    <div  v-show="unCompletedHomeworks" class="grid grid-cols-3 text-left font-bold mb-3">
+      <p>Name</p>
+      <p class="text-center">Category</p>
+      <div class="flex justify-between">
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+    <ul  v-for="item in HomeWorks.filter(item => !item.status)" :key="item.id" class="grid grid-cols-3">
 
-      <label >
-        <li class="flex justify-between items-center mr-4 font-bold">
-           {{item.name}}
-          <input type="checkbox" v-model="item.status" class="ml-2">
-        </li>
-      </label>
-      <button class="font-bold text-base text-red-800"  @click="delete HomeWorks[item.id-1]">Delete</button>
+
+      <li class="flex justify-between items-center mr-4 ">{{item.name}}</li>
+      <p class="text-center"> {{item.category}}</p>
+      <div class="flex justify-between">
+        <input type="checkbox" v-model="item.status" class="ml-2">
+        <button class="font-bold text-base text-red-800"  @click="delete HomeWorks[item.id-1]">Delete</button>
+      </div>
     </ul>
   </div>
 
@@ -81,16 +93,24 @@ function AddTask(HomeWorkAdd ){
 
 
   <div>
-    <h2 class="font-bold mb-2 text-left" >Done</h2>
-    <ul  v-show="CompletedHomeworks" v-for="item in HomeWorks.filter(item => item.status)" :key="item.id" class="grid grid-cols-2 ">
+    <h2 class="font-bold mb-2 text-left" > Done</h2>
+    <div  v-show="CompletedHomeworks" class="grid grid-cols-3 text-left font-bold mb-3">
+      <p>Name</p>
+      <p class="text-center">Category</p>
+      <div class="flex justify-between">
+      <p></p>
+      <p></p>
+      </div>
+    </div>
+    <ul  v-for="item in HomeWorks.filter(item => item.status)" :key="item.id" class="grid grid-cols-3">
 
-      <label >
-        <li class="flex justify-between items-center mr-4 font-bold">
-          {{item.name}}
-          <input type="checkbox" v-model="item.status" class="ml-2">
-        </li>
-      </label>
+
+      <li class="flex justify-between items-center mr-4 ">{{item.name}}</li>
+      <p class="text-center"> {{item.category}}</p>
+      <div class="flex justify-between">
+      <input type="checkbox" v-model="item.status" class="ml-2">
       <button class="font-bold text-base text-red-800"  @click="delete HomeWorks[item.id-1]">Delete</button>
+      </div>
     </ul>
   </div>
 
@@ -101,44 +121,19 @@ function AddTask(HomeWorkAdd ){
 </div>
 
 
-<form @submit.prevent="AddTask(HomeWorkAdd)"  class="flex flex-col mt-4">
+
+<form @submit.prevent="AddTask(HomeWorkAdd ,selected )"  class="flex flex-col mt-4">
     <input type="text" placeholder="Add Task..." class="border mb-3" v-model="HomeWorkAdd">
   <div class="flex">
     <Button title="Add Task" class="w-full mr-1 "/>
-
-    <Menu>
-  <div class="relative">
-      <MenuButton class="bg-white rounded flex items-center   px-3 py-1">
-        <span class="p-1">Category</span>
-        <ChevronDownIcon class="w-4 h-4 ml-1"/>
-      </MenuButton>
-
-      <MenuItems  class="absolute  bg-white rounded-lg shadow-md flex flex-col focus:outline-none w-32 text-left mt-1">
-        <MenuItem  v-slot="{ active }">
-          <button class="text-left px-2 py-2" :class='{ "bg-blue-500 text-white rounded-lg": active }' >University</button>
-        </MenuItem>
-        <MenuItem  v-slot="{ active }">
-          <button class="text-left px-2 py-2" :class='{ "bg-blue-500 text-white rounded-lg": active }' >Work</button>
-        </MenuItem>
-        <MenuItem  v-slot="{ active }">
-          <button class="text-left px-2 py-2" :class='{ "bg-blue-500 text-white rounded-lg": active }' >Personal</button>
-        </MenuItem>
-      </MenuItems>
+    <select v-model="selected" class="rounded p-2 px-5" >
+      <option disabled value="">Category</option>
+      <option v-for="item in Category">{{item}}</option>
+    </select>
   </div>
-    </Menu>
-  </div>
-
   </form>
   <div class="mt-3">
     <Button title="Delete All Tasks" class="bg-red-600 hover:bg-red-800 w-full"  @click="DeleteAllTask"/>
   </div>
-
-
-
-
-
-
-
-
 </template>
 
